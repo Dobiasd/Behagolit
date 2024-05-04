25f5ef638068bfba1ad9f2f9e54f8855aca3e6b6
Dobiasd <editgym@gmail.com>
2024-05-04 16:51:52 +0200

    Add __init__.py

---
+++ compiler/__init__.py
@@ -0,0 +1,12 @@
+from .augmenter import augmenter as augmenter
+from .lexer import lexer as lexer
+from .parser import parser as parser
+from .interpreter import interpret as interpreter
+
+name = "Behagolit"
+
+__author__ = "Tobias Hermann"
+__copyright__ = "Copyright 2024, Tobias Hermann"
+__email__ = "editgym@gmail.com"
+__license__ = "MIT"
+__version__ = "0.0.1"
