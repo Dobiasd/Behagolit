e96355e1e747423e3061b588f8ba92cfc1c821ca
Dobiasd <editgym@gmail.com>
2024-05-12 07:33:04 +0200

    Simplify interpreter

--- compiler/interpreting.py
+++ compiler/interpreting.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import Dict, List, Any, Sequence
+from typing import Dict, List, Any
 
 from .builtins import default_environment
 from .expressions import PrimitiveProcedure, Procedure, Function, Expression, Application, PlainExpression, Variable, \
@@ -23,55 +23,23 @@ def extend_env(env: Dict[str, Expression],
     return env | dict(zip(parameter_names, args))
 
 
-def apply_primitive_procedure(procedure: PrimitiveProcedure, arguments: Sequence[Expression]) -> Expression:
-    return procedure.impl(*list_of_values(procedure.env, arguments))  # type:ignore
-
-
-def make_procedure(parameters: List[Parameter], body: Application, env: Dict[str, Expression]) -> Procedure:
-    return CompoundProcedure(parameters, env, body)
-
-
-def eval_sequence(environment: Dict[str, Expression], application: Application) -> Expression:
-    return evaluate(environment, Application(application.operator, application.operands))
-
-
-# https://wiki.c2.com/?EvalApply
 def apply(procedure: Procedure, arguments: List[Expression]) -> Expression:
     if isinstance(procedure, PrimitiveProcedure):
-        return apply_primitive_procedure(procedure, arguments)
+        return procedure.impl(*list(map(partial(evaluate, procedure.env), arguments)))  # type:ignore
     if isinstance(procedure, CompoundProcedure):
         extended_env = extend_env(procedure.env, procedure.parameters, arguments)
-        return eval_sequence(extended_env, procedure.body)
+        return evaluate(extended_env, Application(procedure.body.operator, procedure.body.operands))
     else:
         raise RuntimeError(f"Unknown function type to apply: {procedure}")
 
 
-def list_of_values(environment: Dict[str, Expression], expressions: Sequence[Expression]) -> List[Expression]:
-    return list(map(partial(evaluate, environment), expressions))
-
-
-def lookup_variable_value(environment: Dict[str, Expression], name: str) -> Expression:
-    exp = environment[name]
-    while isinstance(exp, Variable):
-        if not exp.name in environment:
-            break
-        exp = environment[exp.name]
-    return exp
-
-
-# https://github.com/reah/scheme_interpreter/blob/master/scheme.py
 def evaluate(environment: Dict[str, Expression], expression: Expression) -> Expression:
-    environment = default_environment() | environment
-    # todo: remove
-    print(f"{expression=}")
-    print(f"{environment=}")
     if isinstance(expression, PlainExpression) or isinstance(expression, PrimitiveProcedure):
         return expression
     if isinstance(expression, Variable):
-        return evaluate(environment, lookup_variable_value(environment, expression.name))
+        return evaluate(environment, environment[expression.name])
     if isinstance(expression, Function):
-        # todo: does this ever happen?
-        return make_procedure(expression.parameters, expression.body, environment)
+        return CompoundProcedure(expression.parameters, environment, expression.body)
     if isinstance(expression, CompoundProcedure):
         return expression
     if isinstance(expression, Application):
@@ -86,7 +54,7 @@ def evaluate(environment: Dict[str, Expression], expression: Expression) -> Expr
                 return evaluate(environment, expression.operands[2])
         evaluated_operator = evaluate(environment, expression.operator)
         assert isinstance(evaluated_operator, Procedure)
-        evaluated_operands = list_of_values(environment, expression.operands)
+        evaluated_operands = list(map(partial(evaluate, environment), expression.operands))
         return apply(evaluated_operator, evaluated_operands)
     else:
         raise RuntimeError(f"Unknown expression type: {expression}")
